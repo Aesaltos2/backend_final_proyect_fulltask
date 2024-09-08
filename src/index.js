737db08a4e75251c8955ea01f78cbbe7f629cb85
Adrian Saltos <Asaltos>
2024-09-07 19:23:25 -0500

    Remove database antigua

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 import express from 'express'
 import { PORT } from './config/config.js'
-import { swaggerDocs } from './swagger.js'
 
 import userRoutes from './routes/auserRouters.js'
 import postRoutes from './routes/bpostRoutes.js'
 import categoryRoutes from './routes/categoryRoutes.js'
 import commentRoutes from './routes/commentRoutes.js'
 
-const app = express()
+const app= express()
+app.use(morgan('dev'))
 app.use(express.json())
+app.use(validCors)
 
-// Usar los routers
-app.use('/usuarios', userRoutes)
-app.use('/publicaciones', postRoutes)
-app.use('/categorias', categoryRoutes)
-app.use('/comentarios', commentRoutes)
+app.use('/api/auth',routerAuth)
+app.use('/api/user',routerUser)
+app.use('/api/incident',routerIncident)
+app.use('/api/comment',routerComment)
 
-swaggerDocs(app)
 app.use('*', (req, res) => res.status(404).send('La ruta no existe'))
 
 app.listen(PORT, () => console.log(`Servidor ejecutandose http://localhost:${PORT}`))
